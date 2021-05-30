fe30edcd3dbb2b457236c08f6de88e39083dd093
Amitkumar Mishra <mr.mash@gmail.com>
2021-05-30 16:08:52 +0530

    sidebar copied from master

--- src/components/Main/SideBar.jsx
+++ src/components/Main/SideBar.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setNavigation } from "../../redux/actions/action_types";
+import {ReactComponent as Profile} from '../../user.svg'
+import {ReactComponent as Artists} from '../../microphone-with-wire.svg'
+import {ReactComponent as Recent} from '../../history.svg'
+import {ReactComponent as Playlist} from '../../playlist.svg'
+import {ReactComponent as Tracks} from '../../musical-note.svg'
 
 export default function SideBar() {
+  const dispatch = useDispatch();
+
+  const current = useSelector((state) => state.navigation);
+
   return (
     <div className="side-bar">
       <div className="logo">
@@ -8,32 +19,55 @@ export default function SideBar() {
       </div>
 
       <div className="navigation-links">
-          
-        <div className="nav-link">
-          <img src="./images/user.svg" alt="user" />
-          <p>Profile</p>
+        <div
+          className={`nav-link ${current === "profile" && "active"}`}
+          onClick={() => dispatch(setNavigation("profile"))}
+        >
+          <div className="wrapper" style = {{transform:current === 'profile' && 'scale(1.2)'}}>
+            <Profile fill = {current === 'profile' ? '#ffffff':'#9b9a9d'}/>
+            <p>Profile</p>
+          </div>
         </div>
 
-        <div className="nav-link">
-          <img src="./images/microphone-with-wire.svg" alt="artist" />
-          <p>Artists</p>
+        <div
+          className={`nav-link ${current === "artists" && "active"}`}
+          onClick={() => dispatch(setNavigation("artists"))}
+        >
+          <div className="wrapper" style = {{transform:current === 'artists' && 'scale(1.2)'}}>
+            <Artists fill = {current === 'artists' ? '#ffffff':'#9b9a9d'}/>
+            <p>Artists</p>
+          </div>
         </div>
 
-        <div className="nav-link">
-          <img src="./images/playlist.svg" alt="playlist" />
-          <p>Tracks</p>
+        <div
+          className={`nav-link ${current === "tracks" && "active"}`}
+          onClick={() => dispatch(setNavigation("tracks"))}
+        >
+          <div className="wrapper" style = {{transform:current === 'tracks' && 'scale(1.2)'}}>
+            <Playlist fill = {current === 'tracks' ? '#ffffff':'#9b9a9d'}/>
+            <p>Tracks</p>
+          </div>
         </div>
 
-        <div className="nav-link">
-          <img src="./images/history.svg" alt="recent" />
-          <p>Recent</p>
+        <div
+          className={`nav-link ${current === "recent" && "active"}`}
+          onClick={() => dispatch(setNavigation("recent"))}
+        >
+          <div className="wrapper" style = {{transform:current === 'recent' && 'scale(1.2)'}}>
+            <Recent fill = {current === 'recent' ? '#ffffff':'#9b9a9d'}/>
+            <p>Recent</p>
+          </div>
         </div>
 
-        <div className="nav-link">
-          <img src="./images/musical-note.svg" alt="tracks" />
-          <p>Playlists</p>
+        <div
+          className={`nav-link ${current === "playlists" && "active"}`}
+          onClick={() => dispatch(setNavigation("playlists"))}
+        >
+          <div className="wrapper" style = {{transform:current === 'playlists' && 'scale(1.2)'}}>
+            <Tracks fill = {current === 'playlists' ? '#ffffff':'#9b9a9d'}/>
+            <p>Playlists</p>
+          </div>
         </div>
-
       </div>
 
       <div className="git">
